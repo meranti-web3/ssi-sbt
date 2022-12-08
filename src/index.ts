252eb4cf63404c885af5f50704d2571279ae3d9b
Olivier Scherrer <pode.fr@gmail.com>
2022-12-08 09:37:40 +0100

    Add express route to /mint

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
 import express from "express";
+import bodyParser from "body-parser";
 import type { AddressInfo } from "net";
 
 const app = express();
 
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+app.post("/mint", function (req, res) {
+  res.send(`Minting item with ${JSON.stringify(req.body)}`);
+});
+
 app.get("/", function (req, res) {
-  res.send("Hello World");
+  res.send("you're probably lost here. If not, please contact olivier@meranti.fr");
 });
 
 const server = app.listen(process.env.PORT || 3000, function () {
