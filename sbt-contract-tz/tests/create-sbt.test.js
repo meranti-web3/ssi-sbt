80ba65644ac37a1c70306b65f1b6d9d9e3a44ef3
Olivier Scherrer <pode.fr@gmail.com>
2023-06-12 23:51:21 +0400

    [multi blockchain 03] Update FA2
    - Make tokens non transferable and remove irrelevant code
    - Make token id auto increment

--- sbt-contract-tz/tests/create-sbt.test.js
+++ sbt-contract-tz/tests/create-sbt.test.js
@@ -5,7 +5,7 @@ require("dotenv").config({
 const { TezosToolkit, MichelsonMap } = require("@taquito/taquito");
 const { InMemorySigner } = require("@taquito/signer");
 const { char2Bytes } = require("@taquito/utils");
-const { Tzip12Module, tzip12 } = require("@taquito/tzip12");
+const { Tzip12Module } = require("@taquito/tzip12");
 
 const { BigNumber } = require("bignumber.js");
 const { originateFA2Contract } = require("./utils.js");
@@ -14,7 +14,6 @@ const souldboundTokenContract = require("../build/soulboundToken.json");
 describe("Given SoulboundToken is deployed", () => {
   let soulboundTokenInstance;
   let tezos;
-  let storage;
 
   beforeAll(async () => {
     tezos = new TezosToolkit(process.env.TZ_RPC_URL);
@@ -23,33 +22,63 @@ describe("Given SoulboundToken is deployed", () => {
     tezos.setProvider({
       signer: new InMemorySigner("edsk3QoqBuvdamxouPhin7swCvkQNgq4jP5KZPbwWNnwdZpSpJiEbq")
     });
-
-    soulboundTokenInstance = await originateFA2Contract(tezos, souldboundTokenContract, {
-      administrators: [],
-      ledger: MichelsonMap.fromLiteral({}),
-      metadata: MichelsonMap.fromLiteral({}),
-      token_metadata: MichelsonMap.fromLiteral({}),
-      operators: MichelsonMap.fromLiteral({}),
-      token_ids: []
-    });
-
-    const mintOp = await soulboundTokenInstance.methods
-      .mint(
-        new BigNumber(0),
-        char2Bytes("DeFi"),
-        char2Bytes("Proof of DeFi Compliance"),
-        char2Bytes("DEFI"),
-        char2Bytes("ifps://uri1")
-      )
-      .send();
-
-    await mintOp.confirmation();
-
-    storage = await soulboundTokenInstance.storage();
   });
 
-  it("Then returns the default value", async () => {
-    console.log(await storage.token_metadata.get(0));
-    console.log(await soulboundTokenInstance.tzip12().getTokenMetadata(0));
+  describe("When a new token is minted", () => {
+    beforeAll(async () => {
+      soulboundTokenInstance = await originateFA2Contract(tezos, souldboundTokenContract, {
+        administrators: [],
+        ledger: MichelsonMap.fromLiteral({}),
+        metadata: MichelsonMap.fromLiteral({}),
+        token_metadata: MichelsonMap.fromLiteral({}),
+        operators: MichelsonMap.fromLiteral({}),
+        token_ids: []
+      });
+
+      const mintOp = await soulboundTokenInstance.methods
+        .mint(char2Bytes("DeFi"), char2Bytes("Proof of DeFi Compliance"), char2Bytes("DEFI"), char2Bytes("ifps://uri1"))
+        .send();
+
+      await mintOp.confirmation();
+    });
+
+    it("Then returns its metadata", async () => {
+      expect(await soulboundTokenInstance.tzip12().getTokenMetadata(0)).toEqual({
+        token_id: 0,
+        uri: "ifps://uri1",
+        decimals: 0,
+        description: "Proof of DeFi Compliance",
+        interfaces: '["TZIP-12"]',
+        name: "DeFi",
+        symbol: "DEFI"
+      });
+    });
+
+    describe("When minting a 2nd token", () => {
+      beforeAll(async () => {
+        const mintOp = await soulboundTokenInstance.methods
+          .mint(
+            char2Bytes("DeFi"),
+            char2Bytes("Proof of DeFi Compliance"),
+            char2Bytes("DEFI"),
+            char2Bytes("ifps://uri2")
+          )
+          .send();
+
+        await mintOp.confirmation();
+      });
+
+      it("Then returns its metadata", async () => {
+        expect(await soulboundTokenInstance.tzip12().getTokenMetadata(1)).toEqual({
+          token_id: 1,
+          uri: "ifps://uri2",
+          decimals: 0,
+          description: "Proof of DeFi Compliance",
+          interfaces: '["TZIP-12"]',
+          name: "DeFi",
+          symbol: "DEFI"
+        });
+      });
+    });
   });
 });
